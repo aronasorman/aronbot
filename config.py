b090b02b29ea49a380b8bee37b74ac3e51a3acbf
Aron Fyodor Asor <191955+aronasorman@users.noreply.github.com>
2018-08-14 13:26:52 -0700

    initialize errbot config

---
+++ config.py
@@ -0,0 +1,19 @@
+import logging
+import pathlib
+
+# This is a minimal configuration to get you started with the Text mode.
+# If you want to connect Errbot to chat services, checkout
+# the options in the more complete config-template.py from here:
+# https://raw.githubusercontent.com/errbotio/errbot/master/errbot/config-template.py
+
+BACKEND = 'Text'  # Errbot will start in text mode (console only mode) and will answer commands from there.
+
+ROOT_DIR = pathlib.Path(__file__).parent
+BOT_DATA_DIR =  ROOT_DIR / "data"  # get config.py's dir
+BOT_EXTRA_PLUGIN_DIR = ROOT_DIR / "plugins"
+
+BOT_LOG_FILE = ROOT_DIR / "error.log"
+BOT_LOG_LEVEL = logging.INFO
+
+BOT_ADMINS = ('@aron', '@jamie', '@jordan', '@kollivier')
+BOT_ALT_PREFIXES = ('@aronbot', '@ar0n')
